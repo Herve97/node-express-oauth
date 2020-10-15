7aad52b1923043e3de258bc23d25abe718de4c34
Herve97 <53498298+Herve97@users.noreply.github.com>
2020-10-16 01:42:41 +0200

    Added my project

---
+++ authorization-server.js
@@ -0,0 +1,74 @@
+const fs = require("fs")
+const express = require("express")
+const bodyParser = require("body-parser")
+const jwt = require("jsonwebtoken")
+const {
+	randomString,
+	containsAll,
+	decodeAuthCredentials,
+	timeout,
+} = require("./utils")
+
+const config = {
+	port: 9001,
+	privateKey: fs.readFileSync("assets/private_key.pem"),
+
+	clientId: "my-client",
+	clientSecret: "zETqHgl0d7ThysUqPnaFuLOmG1E=",
+	redirectUri: "http://localhost:9000/callback",
+
+	authorizationEndpoint: "http://localhost:9001/authorize",
+}
+
+const clients = {
+	"my-client": {
+		name: "Sample Client",
+		clientSecret: "zETqHgl0d7ThysUqPnaFuLOmG1E=",
+		scopes: ["permission:name", "permission:date_of_birth"],
+	},
+	"test-client": {
+		name: "Test Client",
+		clientSecret: "TestSecret",
+		scopes: ["permission:name"],
+	},
+}
+
+const users = {
+	user1: "password1",
+	john: "appleseed",
+}
+
+const requests = {}
+const authorizationCodes = {}
+
+let state = ""
+
+const app = express()
+app.set("view engine", "ejs")
+app.set("views", "assets/authorization-server")
+app.use(timeout)
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+/*
+Your code here
+*/
+app.route('/authorize')
+	.get((req, res) => {
+		res.end(200);
+		const clientId = req.query.client_id;
+		if(clients != clientId) {
+			return res.status(401);
+		} else {
+			return res.status(200);
+		}
+	});
+
+const server = app.listen(config.port, "localhost", function () {
+	var host = server.address().address
+	var port = server.address().port
+})
+
+// for testing purposes
+
+module.exports = { app, requests, authorizationCodes, server }
